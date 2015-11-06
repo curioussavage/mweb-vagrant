ed02c8558e73ebf2d57a040fbac4e6c400e55173
adam <adam@reddit.com>
2015-11-06 13:35:32 -0800

    change order

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository ppa:chris-lea/node.js -y
-sudo apt-get update -y
-sudo apt-get install nodejs -y
-
 mkdir /home/vagrant/npm-global
 echo "export NPM_CONFIG_PREFIX=/home/vagrant/npm-global" >> /home/vagrant/.profile
 source /home/vagrant/.profile
+
+sudo add-apt-repository ppa:chris-lea/node.js -y
+sudo apt-get update -y
+sudo apt-get install nodejs -y
 
 npm install -g n
 n v4.2.1

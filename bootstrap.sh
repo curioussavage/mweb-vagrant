0322a858312df0665ed5e6e268f7d4a02ec9df4c
adam <adam@reddit.com>
2015-11-06 12:33:39 -0800

    use n to upgrade node

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository ppa:chris-lea/node.js 
-sudo apt-get update
-sudo apt-get install nodejs
+sudo add-apt-repository ppa:chris-lea/node.js -y
+sudo apt-get update -y
+sudo apt-get install nodejs -y
+
+sudo chown -R vagrant /usr/lib/node_modules/
+sudo chown -R vagrant /usr/bin
+sudo chown -R vagrant /usr/share
+
+npm install -g n
+n use v4.2.1
